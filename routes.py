c43401444d0b126c2968bbdf77e150d9de112b4f
rox51124 <roxane.baert@epitech.eu>
2022-03-26 19:07:36 +0100

    ADD: music

--- routes.py
+++ routes.py
@@ -15,3 +15,15 @@ def game():
 def game_blue_guys():
     #blue_guys = BlueStuff.query.filter_by(userid= current user id )
     return render_template("game/home.html")
+
+@app.route('/music/')
+def music():
+    return render_template("music/home.html")
+
+@app.route('/perso/')
+def perso():
+    return render_template("perso/home.html")
+
+@app.route('/defi/')
+def defi():
+    return render_template("defi/home.html")
